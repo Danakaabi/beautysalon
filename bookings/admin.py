4d735a18e62da98eae3a8a771e72c91b35d4145d
dana <dana1417.80.02@gmail.com>
2026-06-09 05:35:50 +0300

    chore: update booking admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import TimeSlot, Booking, BookingItem
 
-# Register your models here.
+
+@admin.register(TimeSlot)
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = ("id", "time")
+    ordering = ("time",)
+
+
+class BookingItemInline(admin.TabularInline):
+    model = BookingItem
+    extra = 0
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "date", "time", "staff_member", "status", "total_amount")
+    list_filter = ("status", "date", "staff_member")
+    search_fields = ("user__phone", "staff_member__name")
+    inlines = [BookingItemInline]
+
+
+@admin.register(BookingItem)
+class BookingItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "booking", "service", "price_at_booking", "duration_at_booking")
