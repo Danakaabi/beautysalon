3e2472b44a8fae15e63ff107ed12d886b473a405
dana <dana1417.80.02@gmail.com>
2026-06-09 05:36:28 +0300

    chore: update scheduling admin

--- scheduling/admin.py
+++ scheduling/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Staff, StaffService
 
-# Register your models here.
+
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "phone", "is_active")
+    list_filter = ("is_active",)
+    search_fields = ("name", "phone")
+
+
+@admin.register(StaffService)
+class StaffServiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "staff", "service")
